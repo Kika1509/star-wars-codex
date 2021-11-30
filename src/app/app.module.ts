f013fc77ea78134d9d92dcdb1161aeb1841f562e
Kristina Roskar <kristina.roskar@snt.hr>
2021-11-30 10:59:49 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './characters/characters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent
   ],
   imports: [
     BrowserModule
